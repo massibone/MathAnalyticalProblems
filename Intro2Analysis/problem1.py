4f92cc91ea09e5d66f1c0be21ef7b2c46aba1f60
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-15 09:52:10 +0200

    Create problem1.py

---
+++ Intro2Analysis/problem1.py
@@ -0,0 +1,3 @@
+Trova il limite dei perimetri dei poligoni regolari di n lati inscritti nel cerchio di raggio R e circoscritti intorno ad esso, quando n -> ∞
+
+Quando n tende all'infinito, il poligono regolare si avvicina sempre di più a una circonferenza, e il limite del perimetro di questi poligoni sarà uguale al perimetro della circonferenza (2 * π * R).
