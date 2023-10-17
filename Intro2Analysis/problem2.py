2c135a8feea8a882db9b37e35fd2fd071da2e112
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-17 08:03:10 +0200

    Create problem2.py

---
+++ Intro2Analysis/problem2.py
@@ -0,0 +1,13 @@
+'''
+trovare il limite della somma delle lunghezze delle ordinate della curva y = e^{-x} \cos(\pi x)y=e **−x  cos π x 
+Per calcolare il limite della somma delle lunghezze delle ordinate della curva y = e^{-x} \cos(\pi x)y=e 
+−x
+ cos(πx) quando nn tende a infinito, possiamo considerare che le ordinate della curva sono definite nei punti x = 0, 1, 2, \ldots, nx=0,1,2,…,n. Possiamo approssimare la somma delle lunghezze delle ordinate con una somma di Riemann.
+
+L'incremento \Delta xΔx tra due punti adiacenti sarà 11, poiché abbiamo punti equispaziati.
+l limite della somma delle lunghezze delle ordinate della curva y = e^{-x} \cos(\pi x)y=e 
+−x
+ cos(πx) quando nn tende a infinito è 
+e/e−1
+
+'''
