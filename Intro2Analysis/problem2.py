13233c08f98e5e5b4cff511f0834c9184141a5aa
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-17 08:03:30 +0200

    Update problem2.py

--- Intro2Analysis/problem2.py
+++ Intro2Analysis/problem2.py
@@ -11,3 +11,20 @@ l limite della somma delle lunghezze delle ordinate della curva y = e^{-x} \cos(
 e/e−1
 
 '''
+import math
+import matplotlib.pyplot as plt
+
+def function(x):
+    return math.exp(-x) * math.cos(math.pi * x)
+
+def riemann_sum(n):
+    sum = 0
+    x_values = []
+    y_values = []
+    delta_x = 1.0 / n
+    for i in range(n + 1):
+        x = i * delta_x
+        sum += math.sqrt(1 + math.exp(-2 * x))
+        x_values.append(x)
+        y_values.append(function(x))
+    return sum, x_values, y_values
