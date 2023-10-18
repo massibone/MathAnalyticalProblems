6f9c6c97b45cb8c513ec5bdb89e7b5abc71d6620
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-18 08:03:54 +0200

    Create problem3.py

---
+++ Intro2Analysis/problem3.py
@@ -0,0 +1,6 @@
+'''
+come si trasforma in frazione ordinaria la frazione periodica mista alfa=0,13555...., considerandola come limite della corrispondente frazione finita?
+la parte intera e la parte frazionaria periodica. Ad esempio, se hai α = 0,13555..., scriveresti α come α = 0 + 0,13555....
+α = 0,13555... = 0 + 0,13555... = 0 + 0,13555... = 0 + 13/100 + 555/10000
+α = 130555 / 1000000 = 661/5000
+'''
