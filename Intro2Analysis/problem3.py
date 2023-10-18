be656d4c8a8c9f0961245a7d46adf66dc5fff1eb
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-18 08:04:11 +0200

    Update problem3.py

--- Intro2Analysis/problem3.py
+++ Intro2Analysis/problem3.py
@@ -4,3 +4,16 @@ la parte intera e la parte frazionaria periodica. Ad esempio, se hai α = 0,1355
 α = 0,13555... = 0 + 0,13555... = 0 + 0,13555... = 0 + 13/100 + 555/10000
 α = 130555 / 1000000 = 661/5000
 '''
+
+def frazione_periodica_mista_to_frazione_ordinaria(parte_intera, parte_periodica):
+    parte_intera = int(parte_intera)
+    parte_periodica = parte_periodica.rstrip('0')  # Rimuovi zeri finali dalla parte periodica
+    parte_periodica_int = int(parte_periodica)
+    
+    lunghezza_periodo = len(parte_periodica)
+    denominatore = 10 ** lunghezza_periodo - 1
+    
+    numeratore = parte_intera * denominatore + parte_periodica_int
+    frazione_ordinaria = numeratore / denominatore
+    
+    return frazione_ordinaria
